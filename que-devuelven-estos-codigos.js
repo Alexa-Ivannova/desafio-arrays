4d50654991a1701268a2773ac5569af00f87c7a8
Alexa Bautista <alexa.bautista@genstudents.org>
2024-08-12 21:17:25 -0500

    Ejercicios de analisis y predicción de codigo basicos

---
+++ que-devuelven-estos-codigos.js
@@ -0,0 +1,25 @@
+// ¿Qué devuelven estos códigos? 
+
+// ACCESO FUER DEL INDICE:
+// En este ejercicio deberás pensar qué devuelven estos códigos sin probarlos en la consola. 
+// let numbers =[22, 33, 54, 66, 72] 
+// console.log(numbers[numbers.length]) 
+
+// RESPUESTA: Considero que va a mostrar UNDEFINED, ya que number.lenght hace referencia a la longitud del array "numbers" que equivale a 5, y el indice siempre inicia en 0, en este caso el indice final es 4, lo que hace que el indice 5 este indefinido. 
+// => undefined
+
+// ACCESO A ELEMENTOS ESPECÍFICOS EN UN ARRAY DE STRINGS:
+// let grupoDeAmigos = [ "Harry", "Ron", "Hermione", "Spiderman", "Hulk", "Ironman","Penélope Glamour", "Pierre Nodoyuna","Patán" ] 
+// console.log(grupoDeAmigos[5])
+
+// RESPUESTA: Mostrara el string "Ironman", ya que el conteo del indice inicia en 0, y al llegar al indice 5 encontramos el string "Ironman"
+// => Ironman
+
+// USO DE VARIABLES EN ARRAYS:
+// 3. let str = "un string cualquiera" 
+// let arrayAleatorio= ["Digital", "House", "true", "string", "123","false", "54", str ] 
+// console.log(arrayAleatorio[arrayAleatorio.length - 1]) 
+
+// RESPUESTA: El resulado sera el string de la variable str es decir “un string cualquiera”
+// => un string cualquiera
+
